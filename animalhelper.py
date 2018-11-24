a173703eb2e677540459d7bb4728032c3ffffee4
luketibbott <luke.tibbott@gmail.com>
2018-11-23 17:00:50 -0800

    Extract datetime month and season

--- animalhelper.py
+++ animalhelper.py
@@ -2,6 +2,7 @@
 
 import re
 import numpy as np
+import pandas as pd
 
 def get_days(s):
     # takes string s in format similar to "11 years" and returns an int that represents the number of days described in s
@@ -46,3 +47,25 @@ def get_fixed_status(s):
         return 'fixed'
     else:
         return 'unknown'
+
+def get_season(d):
+    d = pd.to_datetime(d)
+
+    month = d.month
+
+    winter = {1, 2, 3}
+    spring = {4, 5, 6}
+    summer = {7, 8, 9}
+    fall = {10, 11, 12}
+
+    if month in winter:
+        return 'winter'
+
+    elif month in spring:
+        return 'spring'
+
+    elif month in summer:
+        return 'summer'
+        
+    else:
+        return 'fall'
