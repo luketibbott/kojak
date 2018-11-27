b697d63868d31257a6f4bace696feab927ff112d
luketibbott <luke.tibbott@gmail.com>
2018-11-27 14:56:33 -0800

    Document breeds.py

--- breeds.py
+++ breeds.py
@@ -1,3 +1,6 @@
+# Parses a wikipedia page listing dog breeds recognized by the American Kennel Club and the
+# group of dog each breed belongs to.
+
 import wikipedia
 
 dog_breeds = wikipedia.page('List of dog breeds recognized by the American Kennel Club')
@@ -5,9 +8,11 @@ dog_breeds = wikipedia.page('List of dog breeds recognized by the American Kenne
 counter = 0
 dog_groups = dict()
 
+
 for line in dog_breeds.content.split('\n')[1:]:
     counter += 1
     split = False
+    # Catch text error with no space after comma
     if (', ' in line) and (counter < 272):
         line = line.split(', ')
         split = True
@@ -15,9 +20,8 @@ for line in dog_breeds.content.split('\n')[1:]:
     elif (',' in line) and (counter < 272):
         line = line.split(',')
         split = True
-    
+    # Split being true means the line we're interested in has dog breed info
     if split:
         dog_groups[line[0]] = line[1]
 
 print(dog_groups)
-print(len(dog_groups))
